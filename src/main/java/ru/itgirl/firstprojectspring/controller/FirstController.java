a35580b30a700332a0bcf559c1c11319844a2df4
AnnTarar <127330046+AnnTarar@users.noreply.github.com>
2023-07-01 13:48:18 +0300

    Add files via upload

---
+++ src/main/java/ru/itgirl/firstprojectspring/controller/FirstController.java
@@ -0,0 +1,45 @@
+package ru.itgirl.firstprojectspring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+//http://localhost:8080/dayOfWeek?day=
+@RestController
+public class FirstController {
+    @GetMapping("/dayOfWeek")
+    public String getWeekday(@RequestParam(value = "day", defaultValue = "Такого дня нет") String day) {
+        String russianDay = "";
+        switch (day) {
+            case "Monday":
+                russianDay = "Понедельник";
+                break;
+            case "Tuesday":
+                russianDay = "Вторник";
+                break;
+            case "Wednesday":
+                russianDay = "Среда";
+                break;
+            case "Thursday":
+                russianDay = "Четверг";
+                break;
+            case "Friday":
+                russianDay = "Пятница";
+                break;
+            case "Saturday":
+                russianDay = "Суббота";
+                break;
+            case "Sunday":
+                russianDay = "Воскресенье";
+                break;
+            default:
+                russianDay = "Такого дня недели нет";
+                break;
+
+        }
+        return russianDay;
+    }
+}
+
